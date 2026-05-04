fd3411ae3ce9ab477e710c457c86f9bd020d4253
Ryuya Ikeda <rikeda71@gmail.com>
2026-05-05 02:24:53 +0900

    feat(main): 新規 contexts を Provider ツリーに統合
    Wave-Gap-3/4 でポートした SessionProvider, OperatorProvider,
    RFIDCardMappingProvider, InitialBoardProvider, CardPlacedEventProvider,
    TelopProvider を main.tsx の Provider ツリーに組み込み、
    全ページから useSession/useOperator/useTelop 等のフックが
    利用可能な状態にした。

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,12 @@ import { RouterProvider } from "react-router";
 import { AuthProvider } from "./contexts/auth_context";
 import { AutoBoardProvider } from "./contexts/auto_board_context";
 import { BoardProvider } from "./contexts/board_context";
+import { CardPlacedEventProvider } from "./contexts/card_placed_event";
+import { InitialBoardProvider } from "./contexts/initial_board_context";
+import { OperatorProvider } from "./contexts/operator_context";
+import { RFIDCardMappingProvider } from "./contexts/rfid_card_mapping_context";
+import { SessionProvider } from "./contexts/session_context";
+import { TelopProvider } from "./contexts/telop_context";
 import { mainRouter } from "./routes";
 
 const rootElement = document.getElementById("root") as HTMLElement;
@@ -13,12 +19,24 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <AuthProvider>
-      <BoardProvider>
-        <AutoBoardProvider>
-          <RouterProvider router={mainRouter} />
-          <Toaster />
-        </AutoBoardProvider>
-      </BoardProvider>
+      <SessionProvider>
+        <OperatorProvider>
+          <RFIDCardMappingProvider>
+            <InitialBoardProvider>
+              <CardPlacedEventProvider>
+                <TelopProvider>
+                  <BoardProvider>
+                    <AutoBoardProvider>
+                      <RouterProvider router={mainRouter} />
+                      <Toaster />
+                    </AutoBoardProvider>
+                  </BoardProvider>
+                </TelopProvider>
+              </CardPlacedEventProvider>
+            </InitialBoardProvider>
+          </RFIDCardMappingProvider>
+        </OperatorProvider>
+      </SessionProvider>
     </AuthProvider>
   </StrictMode>,
 );
